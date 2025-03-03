e43099e888c36f7c1060927b04a1cf4bc08f896e
Alex Gitonga <alexkamau309@gmail.com>
2025-03-03 21:54:41 +0300

    second commit

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import FLASK
+app=FLASK(__name__)
 
-app=Flask(__name__)
+
 @app.route("/")
 def hello():
     return "hello world"
+
 if __name__=="_main_":
     app.run(debug=True)
